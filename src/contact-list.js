ab0c2df6f7036226b41de211cb86b45b69ec8149
Jason Adkison <jadkison@gmail.com>
2016-12-07 11:24:55 -0600

    working contact manager

---
+++ src/contact-list.js
@@ -0,0 +1,32 @@
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { WebAPI } from './web-api';
+import { ContactUpdated, ContactViewed } from './messages';
+
+export class ContactList {
+
+  static inject() {
+    return [WebAPI, EventAggregator];
+  }
+
+  constructor(api, ea) {
+    this.api = api;
+    this.contacts = [];
+
+    ea.subscribe(ContactViewed, msg => this.select(msg.contact));
+    ea.subscribe(ContactUpdated, msg => {
+      const { id } = msg.contact;
+      const found = this.contacts.find(x => x.id === id);
+      Object.assign(found, msg.contact);
+    });
+  }
+
+  created() {
+    this.api.getContactList().then(contacts => this.contacts = contacts);
+  }
+
+  select(contact) {
+    this.selectedId = contact.id;
+    return true;
+  }
+
+}
